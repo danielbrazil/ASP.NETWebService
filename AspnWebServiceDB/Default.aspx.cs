dcb718d149e6b5ee4804b73c0ba2a57766720d97
Daniel Brazil <danielbrazil@hotmail.com>
2019-08-22 12:04:59 -0400

    New Files
    Files

---
+++ AspnWebServiceDB/Default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AspnWebServiceDB
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                this.VinculaDados();
+            }
+        }
+
+        private void VinculaDados()
+        {
+            ServiceDB service = new ServiceDB();
+            gdvProdutos.DataSource = service.GetDados();
+            gdvProdutos.DataBind();
+        }
+    }
+}
